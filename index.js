ead25315a746de145d0987daf52cedf1cbe0766a
korya <korya@github.com>
2016-06-07 00:09:09 -0400

    Basic implementation

---
+++ index.js
@@ -0,0 +1,118 @@
+'use strict';
+
+var through = require('through2');
+var gutil = require('gulp-util');
+var extend = require('object-assign');
+var yaml = require('js-yaml');
+var BufferStreams = require('bufferstreams');
+var jb = require('js-beautify').js_beautify;
+var toSingleQuotes = require('to-single-quotes-shahata');
+var multiline = require('multiline');
+
+var PLUGIN_NAME = 'plntr-locale';
+
+var templateStr = multiline(function () {/*
+'use strict';
+
+angular.module('<%= moduleName %>')
+  .service('locale.<%= language %>', function () {
+    var locale = <%= text %>;
+    return locale;
+  });
+*/
+});
+
+module.exports = function (options) {
+  var extractLanguage;
+
+  options = extend({
+    moduleName: 'app.i18n',
+    extractLanguage: /[^_]*(?=\.[^.]*$)/,
+  }, options);
+
+  if (typeof(options.extractLanguage) === 'function') {
+    extractLanguage = options.extractLanguage;
+  } else {
+    extractLanguage = function (filepath) {
+      return filepath.match(options.extractLanguage)[0];
+    };
+  }
+
+  return through.obj(function(file, enc, cb) {
+
+    if (file.isBuffer()) {
+      if (file.contents.length === 0) {
+        this.emit('error', new gutil.PluginError(PLUGIN_NAME,
+          'File ' + file.path + ' is empty'));
+        return cb();
+      }
+
+      try {
+        file.contents = createLocaleService(file.contents, {
+          file: file,
+          moduleName: options.moduleName,
+          language: extractLanguage(file.path),
+        });
+        file.path = gutil.replaceExtension(file.path, '.js');
+      } catch (error) {
+        this.emit('error', new gutil.PluginError(PLUGIN_NAME,
+          error, {showStack: true}));
+        return cb();
+      }
+
+      this.push(file);
+      cb();
+      return;
+    }
+
+    if (file.isStream()) {
+      var _this = this;
+      var streamer = new BufferStreams(function(err, buf, cb) {
+        if (err) {
+          _this.emit('error', new gutil.PluginError(PLUGIN_NAME,
+            err, {showStack: true}));
+          return;
+        }
+
+        if (buf.length === 0) {
+          _this.emit('error', new gutil.PluginError(PLUGIN_NAME,
+            'File ' + file.path + ' is empty'));
+          return;
+        }
+
+        try {
+          var parsed = createLocaleService(buf, {
+            file: file,
+            moduleName: options.moduleName,
+            language: extractLanguage(file.path),
+          });
+          file.path = gutil.replaceExtension(file.path, '.js');
+          cb(null, parsed);
+        } catch (error) {
+          _this.emit('error', new gutil.PluginError(PLUGIN_NAME,
+            error, {showStack: true}));
+          return;
+        }
+      });
+
+      file.contents = file.contents.pipe(streamer);
+
+      this.push(file);
+      cb();
+      return;
+    }
+
+    this.push(file);
+    cb();
+  });
+};
+
+function createLocaleService(buffer, options) {
+  var obj = yaml.safeLoad(buffer.toString('utf8'));
+
+  var src = gutil.template(templateStr, extend(options, {
+    text: toSingleQuotes(JSON.stringify(obj)),
+  }));
+
+  return new Buffer(jb(src, {'indent_size': 2, 'jslint_happy': true}));
+}
---
+++ test/main.js
@@ -0,0 +1,303 @@
+/* jshint node: true */
+/* global describe, it, beforeEach */
+'use strict';
+
+var plntrLocale = require('../');
+var assert = require('assert');
+var gutil = require('gulp-util');
+var path = require('path');
+var es = require('event-stream');
+var File = require('vinyl');
+var multiline = require('multiline');
+var Readable = require('stream').Readable;
+require('mocha');
+
+describe('gulp-plntr-locale', function () {
+
+  describe('in buffer mode', function () {
+
+    var _createFile = function (contents, filename) {
+      if (typeof contents === 'string') {
+        contents = new Buffer(contents, 'utf8');
+      } else if (Array.isArray(contents)) {
+        contents = new Buffer(contents.join('\n'), 'utf8');
+      }
+      return new File({
+        cwd: './',
+        base: './test/',
+        path: './test/' + (filename || 'mock.yml'),
+        contents: contents
+      });
+    };
+
+    var _fileContents = function (file) {
+      return file.contents ? file.contents.toString('utf8') : null;
+    };
+
+    it('default settings', function (done) {
+      var stream = plntrLocale();
+
+      stream.once('data', function (file) {
+        assert.equal(file.extname, '.js');
+        assert.equal(_fileContents(file), multiline(function () {/*
+'use strict';
+
+angular.module('app.i18n')
+  .service('locale.foo', function () {
+    var locale = {
+      'a': 'b',
+      'c': {
+        'x': 'd',
+        'y': 'e'
+      }
+    };
+    return locale;
+  });
+*/
+        }));
+        done();
+      });
+
+      stream.write(_createFile([
+        'a: b',
+        'c:',
+        '  x: d',
+        '  y: e',
+      ], 'messages_foo.js'));
+      stream.end();
+    });
+
+    it('custom settings', function (done) {
+      var stream = plntrLocale({
+        moduleName: 'moda',
+        extractLanguage: function (filepath) {
+          return '__' + path.basename(filepath) + '__';
+        },
+      });
+
+      stream.once('data', function (file) {
+        assert.equal(file.extname, '.js');
+        assert.equal(_fileContents(file), multiline(function () {/*
+'use strict';
+
+angular.module('moda')
+  .service('locale.__messages_foo.js__', function () {
+    var locale = {
+      'a': 'b',
+      'c': {
+        'x': 'd',
+        'y': 'e'
+      }
+    };
+    return locale;
+  });
+*/
+        }));
+        done();
+      });
+
+      stream.write(_createFile([
+        'a: b',
+        'c:',
+        '  x: d',
+        '  y: e',
+      ], 'messages_foo.js'));
+      stream.end();
+    });
+
+    it('should throw if empty file', function (done) {
+      var stream = plntrLocale();
+
+      stream.once('error', function(err) {
+        assert.ok(err instanceof gutil.PluginError);
+        done();
+      });
+
+      stream.write(_createFile(''));
+      stream.end();
+    });
+
+    it('should throw if not well formatted', function (done) {
+      var stream = plntrLocale();
+
+      stream.once('error', function(err) {
+        assert.ok(err instanceof gutil.PluginError);
+        done();
+      });
+
+      stream.write(_createFile([
+        '$',
+        'missing:',
+        '  something?'
+      ]));
+      stream.end();
+    });
+
+    it('should use safe loading by default', function(done) {
+      var stream = plntrLocale({safe: true});
+
+      stream.once('error', function(err) {
+        assert.ok(err instanceof gutil.PluginError);
+        done();
+      });
+
+      stream.write(_createFile('"toString": !<tag:yaml.org,2002:js/function> "function (){very_evil_thing();}"'));
+      stream.end();
+    });
+
+  });
+
+  describe('in stream mode', function() {
+
+    var _createFile = function(callback, filename) {
+      if (arguments.length === 1) {
+        callback = arguments[0];
+        filename = null;
+      }
+      var stream = new Readable();
+      stream._read = function() {
+        callback.apply(this, arguments);
+        this.push(null);
+      };
+      return new File({
+        cwd: './',
+        base: './test/',
+        path: './test/' + (filename || 'mock.yml'),
+        contents: stream
+      });
+    };
+
+    it('default settings', function (done) {
+      var stream = plntrLocale();
+
+      stream.once('data', function (file) {
+        file.contents.pipe(es.wait(function (err, data) {
+          assert.equal(file.extname, '.js');
+          assert.equal(data.toString('utf8'), multiline(function () {/*
+'use strict';
+
+angular.module('app.i18n')
+  .service('locale.foo', function () {
+    var locale = {
+      'a': 'b',
+      'c': {
+        'x': 'd',
+        'y': 'e'
+      }
+    };
+    return locale;
+  });
+*/
+          }));
+          done();
+        }));
+      });
+
+      stream.write(_createFile(function () {
+        this.push('a: b\n');
+        this.push('c:\n');
+        this.push('  x: d\n');
+        this.push('  y: e\n');
+      }, 'messages_foo.js'));
+      stream.end();
+    });
+
+    it('custom settings', function (done) {
+      var stream = plntrLocale({
+        moduleName: 'moda',
+        extractLanguage: function (filepath) {
+          return '__' + path.basename(filepath) + '__';
+        },
+      });
+
+      stream.once('data', function (file) {
+        file.contents.pipe(es.wait(function (err, data) {
+          assert.equal(file.extname, '.js');
+          assert.equal(data.toString('utf8'), multiline(function () {/*
+'use strict';
+
+angular.module('moda')
+  .service('locale.__messages_foo.js__', function () {
+    var locale = {
+      'a': 'b',
+      'c': {
+        'x': 'd',
+        'y': 'e'
+      }
+    };
+    return locale;
+  });
+*/
+          }));
+          done();
+        }));
+      });
+
+      stream.write(_createFile(function () {
+        this.push('a: b\n');
+        this.push('c:\n');
+        this.push('  x: d\n');
+        this.push('  y: e\n');
+      }, 'messages_foo.js'));
+      stream.end();
+    });
+
+    it('should throw if empty file', function (done) {
+      var stream = plntrLocale();
+
+      stream.once('error', function (err) {
+        assert.ok(err instanceof gutil.PluginError);
+        done();
+      });
+
+      stream.write(_createFile(function() { }));
+      stream.end();
+    });
+
+    it('should throw if not well formatted', function (done) {
+      var stream = plntrLocale();
+
+      stream.once('error', function (err) {
+        assert.ok(err instanceof gutil.PluginError);
+        done();
+      });
+
+      stream.write(_createFile(function () {
+        this.push('$\n');
+        this.push('missing:\n');
+        this.push('  something?\n');
+      }));
+      stream.end();
+    });
+
+    it('should throw if loading untrusted document with safe option enabled', function (done) {
+      var stream = plntrLocale({safe: true});
+
+      stream.once('error', function (err) {
+        assert.ok(err instanceof gutil.PluginError);
+        done();
+      });
+
+      stream.write(_createFile(function () {
+        this.push('"toString": !<tag:yaml.org,2002:js/function> "function (){very_evil_thing();}"');
+      }));
+      stream.end();
+    });
+
+    it('should use safe loading by default', function (done) {
+      var stream = plntrLocale();
+
+      stream.once('error', function (err) {
+        assert.ok(err instanceof gutil.PluginError);
+        done();
+      });
+
+      stream.write(_createFile(function () {
+        this.push('"toString": !<tag:yaml.org,2002:js/function> "function (){very_evil_thing();}"');
+      }));
+      stream.end();
+    });
+
+  });
+
+});
